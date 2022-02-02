7bb090e175ee2f9625730443f7d17835839ffbd3
Maxime <maxime.attala@edu.devinci.fr>
2022-02-02 16:41:36 +0100

    more fixes

--- src/App.js
+++ src/App.js
@@ -7,57 +7,57 @@
 
 import Rooter from './routes/Rooter'
 
-const DataRoadMap = [{
-  percentage: '0%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '10%',
-  title: 'Titre 10°',
-  content: 'Content 10°',
-}, {
-  percentage: '20%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '30%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '40%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '50%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '60%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '70%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}, {
-  percentage: '80%',
-  title: 'Titre 1',
-  content: 'Content 1', 
-},{
-  percentage: '90%',
-  title: 'Titre 1',
-  content: 'Content 1',
-},{
-  percentage: '100%',
-  title: 'Titre 1',
-  content: 'Content 1',
-}];
+// const DataRoadMap = [{
+//   percentage: '0%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '10%',
+//   title: 'Titre 10°',
+//   content: 'Content 10°',
+// }, {
+//   percentage: '20%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '30%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '40%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '50%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '60%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '70%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }, {
+//   percentage: '80%',
+//   title: 'Titre 1',
+//   content: 'Content 1', 
+// },{
+//   percentage: '90%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// },{
+//   percentage: '100%',
+//   title: 'Titre 1',
+//   content: 'Content 1',
+// }];
 
 function App() {
-  const user = {
-    firstname : "Lorem",
-    lastname : "ipsum"
-  }
+  // const user = {
+  //   firstname : "Lorem",
+  //   lastname : "ipsum"
+  // }
 
   return (
     <>
